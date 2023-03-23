18e655260a44e7cc4000cb0fe3df7e0ffb203070
Mert Can Güven <c1812030@student.cankaya.edu.tr>
2023-03-23 16:04:11 +0300

    main functionality of the app

--- App.tsx
+++ App.tsx
@@ -14,12 +14,15 @@ function App(): JSX.Element {
     <SafeAreaView style={styles.container}>
       <Text style={{color:'red', fontWeight: 'bold'}}>
         Merhaba
-        <Text> sincapp </Text>
+        <Text onPress={handlePress}> sincapp </Text>
         severler
       </Text>
     </SafeAreaView>
   );
 }
+const handlePress = () => {
+  console.log('You pressed the text!');
+};
 
 const styles = StyleSheet.create({
   container: {
